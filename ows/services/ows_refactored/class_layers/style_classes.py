03b36d0aa5fa893b5fe54e16cf32b7642f3a40f6
DiegoHMM <diegohmarques.ti@gmail.com>
2024-09-26 13:52:57 -0300

    Fix: corrige style_class_simples

--- ows/services/ows_refactored/class_layers/style_classes.py
+++ ows/services/ows_refactored/class_layers/style_classes.py
@@ -5,24 +5,16 @@ style_class_simples = {
     "components": {
         "red": {
             "building": 1.0,
-            "vegetation": 0.0,
-            "background": 0.0
         },
         "green": {
-            "building": 0.0,
             "vegetation": 1.0,
-            "background": 0.0
         },
         "blue": {
-            "building": 0.0,
-            "vegetation": 0.0,
             "background": 1.0
         },
     },
     "scale_range": [0.0, 255.0],
 }
-
-
 
 styles_classes_list = [
     style_class_simples,
