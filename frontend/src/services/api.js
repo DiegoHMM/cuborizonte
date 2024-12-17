c8af47e738377d7a6f9deb35d43fc9cf1213c1d3
DiegoHMM <diegohmarques.ti@gmail.com>
2024-12-17 10:37:16 -0300

    Feat: inclui variaveis de ambiente para producao de dev

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ const baseApiURL = process.env.REACT_APP_API_BASE_URL || '/api/';
 
 // services/api.js
 export const getPixelValues = async (latitude, longitude) => {
-    const response = await fetch(`${baseApiURL}get_pixel_class/`, {
+    const response = await fetch(`${baseApiURL}/get_pixel_class/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -20,7 +20,7 @@ export const getPixelValues = async (latitude, longitude) => {
 
 
 export const get_classified_products = async () => {
-    const response = await fetch(`${baseApiURL}get_classified_products`, {
+    const response = await fetch(`${baseApiURL}/get_classified_products`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -36,7 +36,7 @@ export const get_classified_products = async () => {
 
 
 export const get_ortho_products = async () => {
-    const response = await fetch(`${baseApiURL}get_ortho_products`, {
+    const response = await fetch(`${baseApiURL}/get_ortho_products`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -52,7 +52,7 @@ export const get_ortho_products = async () => {
 
 
 export const get_plan_products = async () => {
-    const response = await fetch(`${baseApiURL}get_plan_products`, {
+    const response = await fetch(`${baseApiURL}/get_plan_products`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
